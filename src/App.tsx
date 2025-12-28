a87c7ecc77f0d4c9b68ff3912b3c5b097d412c32
twoamdev <97489062+twoamdev@users.noreply.github.com>
2025-12-28 14:38:46 -0500

    updated base styling for nodes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
 import { useState, useCallback } from 'react';
-import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge, 
+import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge, Panel, SelectionMode, Controls,
   type Node,
   type Edge,
   type OnNodesChange,
   type OnEdgesChange,
   type OnConnect, } from '@xyflow/react';
-import '@xyflow/react/dist/style.css';
+
+const panOnDrag : number[] = [1, 2];
+const proOptions = { hideAttribution: true };
+import CustomNode from './components/nodes/BaseNode';
+ 
+const nodeTypes = {
+  custom: CustomNode,
+};
  
 const initialNodes : Node[] = [
-  { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
-  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+  {
+    id: '1',
+    type: 'custom',
+    data: { label: 'bty.{$F}.png sequence' , effect: 'File' },
+    position: { x: 0, y: 50 },
+  },
+  {
+    id: '2',
+    type: 'custom',
+    data: { label: 'Blur bg' , effect: 'Blur' },
+ 
+    position: { x: -200, y: 200 },
+  },
+  {
+    id: '3',
+    type: 'custom',
+    data: { label: 'Crop the image' , effect: 'Crop' },
+    position: { x: 200, y: 200 },
+  },
 ];
-const initialEdges : Edge[]= [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+const initialEdges : Edge[]= [];//[{ id: 'n1-n2', source: 'n1', target: 'n2' }];
  
 export default function App() {
   const [nodes, setNodes] = useState<Node[]>(initialNodes);
@@ -31,15 +55,33 @@ export default function App() {
   );
  
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow
+    <div style={{ width: '100vw', height: '100vh' }} className='bg-[#202020]' >
+      <ReactFlow 
         nodes={nodes}
         edges={edges}
+        nodeTypes={nodeTypes}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        panOnScroll
+        selectionOnDrag
+        panOnDrag={panOnDrag}
+        selectionMode={SelectionMode.Partial}
+        proOptions={proOptions}
         fitView
-      />
+      >
+        <Panel position="top-left" className=' text-xs text-[#ADADAD] px-2 py-[.3rem] !m-0 '>Canal</Panel>
+        <Panel position="top-right" className='text-[#ADADAD] !m-0 w-[33vw] h-[100vh]'>
+          <div className='grid grid-rows-2 h-full bg-[#262626]'>
+            <div className='border-b-1 border-[#202020] grid-span-1'></div>
+            <div className='border-t-1 border-[#202020] grid-span-1'></div>
+          </div>
+          
+        </Panel>
+        <Controls className='text-[#ADADAD]'/>
+        
+      </ReactFlow>
+      
     </div>
   );
 }
---
+++ src/components/nodes/BaseNode.tsx
@@ -0,0 +1,43 @@
+import { memo } from 'react';
+import { Handle, Position } from '@xyflow/react';
+
+type NodeData = {
+  label: string;
+  effect: string;
+}
+
+
+
+function BaseNode({ data }: { data: NodeData }) {
+  return (
+    <div className="shadow-md w-[480px] h-[300px] m-[10px] py-[2px]">
+      <div className='h-[9.5%] w-full bg-[#313131] border-2 border-[#555555]'>
+        <div className='w-full h-full flex flex-row justify-between items-center text-[#888888] text-xs px-2'>
+            <div>{data.label}</div>
+            <div className='flex flex-row gap-2'>
+                <div>{data.effect}</div>
+                <div>ⓘ</div>
+            </div>
+            
+        </div>
+        
+      </div>
+      <div className='h-[0.5%] w-full '></div>
+      <div className='h-[90%] w-full bg-[#222222] border-2 border-[#555555]'></div>
+
+      <Handle
+        type="target"
+        position={Position.Top}
+        className="w-[20px] h-[20px] border-2 border-[#888888] !bg-[#555555] !left-[20px]"
+      />
+      <Handle
+        type="source"
+        position={Position.Bottom}
+        className=" border-2 border-[#555555] w-[20px] h-[20px] !bg-[#313131] !left-[20px]"
+      />
+      
+    </div>
+  );
+}
+
+export default memo(BaseNode);
